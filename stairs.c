af8bc193f05ca01f4f98e5e485384a1844d9c95c
johannasmriti <64794456+johannasmriti@users.noreply.github.com>
2020-05-08 20:43:18 +0530

    Created by Johanna
    this function prints a "01" right angled triangle for 6 rows.

---
+++ stairs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int i,j;
+    for(i=0;i<=6;i++)
+    {
+        for(j=0;j<i;j++)
+        {
+            printf("01");
+        }
+        printf("\n");
+    }
+
+}
