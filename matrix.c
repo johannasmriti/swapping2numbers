7d2859c35450835e8f389b087523233c09bb9320
johannasmriti <64794456+johannasmriti@users.noreply.github.com>
2020-05-10 13:22:05 +0530

    Created by Johanna
    This program inputs and prints a 3X3 matrix.

---
+++ matrix.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a[3][3],i,j;
+    printf("Input elements in the matrix: \n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("element-[%d],[%d]: ",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("\n");
+    printf("The matrix is: \n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+}
